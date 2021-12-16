668cea7574ba25e8d466fcd26486abeba9b9e7f0
jimak <jiamak-pg@yandex.by>
2021-12-16 18:00:00 +0300

    feat: add materials

---
+++ паттерны/observer.js
@@ -0,0 +1,20 @@
+class Subject() {
+  constructor() {
+    this.observers = [];
+  }
+
+  subscribe(observer) {
+    this.observers = this.observers.push(observer);
+  }
+
+  unsubscribe(observer) {
+    this.observers = this.observers.filter(obs => obs !== observer);
+  }
+
+  fire(changes) {
+    this.observers.forEach(observer => {
+      observer.update(changes);
+    });
+  }
+}
+
