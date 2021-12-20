3f2f4696f3c64176dd1c57e74b9ff23b55ca0739
JimakPasha <Jimak@tut.by>
2021-12-20 23:26:47 +0300

    feat: add materials

---
+++ задачи.js
@@ -0,0 +1,39 @@
+// https://learn.javascript.ru/bind#tasks
+
+function sayHi() {
+  alert( this.name );
+}
+sayHi.test = 5;
+
+let bound = sayHi.bind({
+  name: "Вася"
+});
+
+alert( bound.test ); // что выведет? почему?
+
+
+// -----
+
+function askPassword(ok, fail) {
+  let password = prompt("Password?", "");
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user = {
+  name: "Вася",
+
+  loginOk() {
+    alert(`${this.name} logged in`);
+  },
+
+  loginFail() {
+    alert(`${this.name} failed to log in`);
+  },
+};
+
+askPassword(user.loginOk, user.loginFail);
+
+
+
+askPassword(user.login.bind(user, result)); // ?
