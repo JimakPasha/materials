9c0c660e1ee65cae41dda482a81be16ca8e5205d
pavel <pavel@pavel.com>
2022-01-10 19:59:47 +0300

    feat: add materials 10.01.21

--- задачи.js
+++ задачи.js
@@ -123,3 +123,22 @@ askPassword(user.login.bind(user, result)); // ?
 +25. swager, postman
 +26. CI CD
 +27. CDN
+
+
+function extraPerfect(n){
+  let arr = [];
+  for (let a = 0; a <= n; a++) {
+    if (a % 2 !==0) {
+      arr.push(a)
+    }
+  }
+  return arr;
+}
+
+
+
+console.log(extraPerfect(3)); // [1,3]
+console.log(extraPerfect(5)); // [1,3,5]
+console.log(extraPerfect(7)); // [1,3,5,7]
+console.log(extraPerfect(28)); // [1,3,5,7,9,11,13,15,17,19,21,23,25,27]
+console.log(extraPerfect(39)); // [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39]
