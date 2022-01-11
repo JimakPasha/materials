cf3e14918e1fc37de9c974955148d0d1162a7fa9
pavel <pavel@pavel.com>
2022-01-11 15:56:29 +0300

    feat: add materials 11.01.22

--- test/index.js
+++ test/index.js
@@ -1,18 +1,22 @@
 function foo() {
-  console.log(this.a);
+	console.log(this.a);
 }
 
 function doFoo(fn) {
-  // `fn` — просто еще одна ссылка на `foo`
+	// `fn` — просто еще одна ссылка на `foo`
 
-  fn(); // <-- точка вызова!
+	fn(); // <-- точка вызова!
 }
 
 var obj = {
-  a: 2,
-  foo: foo,
+	a: 2,
+	foo: foo,
 };
 
 var a = 'ой, глобальная'; // `a` еще и переменная в глобальном объекте
 
 doFoo(obj.foo.call(obj)); // "ой, глобальная"
+
+const f = [234, 24545, 5, 654, 23423].map((i) => +i.toString().padEnd(15, '1'));
+
+console.log(f);
