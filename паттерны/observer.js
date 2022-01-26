093cf304c6b5845d38f3fd3d47f1703773b5eb03
pavel <pavel@pavel.com>
2022-01-26 17:21:16 +0300

    feat: add materials 26.01

--- паттерны/observer.js
+++ паттерны/observer.js
@@ -1,19 +1,23 @@
-class Subject() {
-  constructor() {
-    this.observers = [];
-  }
+// Observer
+// Формирует зависимости 1 к многим.
+// Суть в том, что у нас есть один объект, в котором мы можем затрегерить вызов изменений и дальше все другие объекты, которые подписаны на изменения они получают обновления и делают свой функционал.
 
-  subscribe(observer) {
-    this.observers = this.observers.push(observer);
-  }
+class Subject {
+	constructor() {
+		this.observers = [];
+	}
 
-  unsubscribe(observer) {
-    this.observers = this.observers.filter(obs => obs !== observer);
-  }
+	subscribe(observer) {
+		this.observers = this.observers.push(observer);
+	}
 
-  fire(changes) {
-    this.observers.forEach(observer => {
-      observer.update(changes);
-    });
-  }
+	unsubscribe(observer) {
+		this.observers = this.observers.filter((obs) => obs !== observer);
+	}
+
+	fire(changes) {
+		this.observers.forEach((observer) => {
+			observer.update(changes);
+		});
+	}
 }
