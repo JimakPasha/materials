71105b7fd4db922572ab858f93d40282802c05cf
pavel <pavel@pavel.com>
2022-02-18 17:55:40 +0300

    feat: add materials

--- _js/script-2.24(практика-4).js
+++ _js/script-2.24(практика-4).js
@@ -11,83 +11,79 @@
 
 'use strict';
 
-
 const personalMovieDB = {
-  count: 0,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-  
-  start: function () {
-    personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+	count: 0,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
 
-    while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-      personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-  },
-  
-  rememberMyFilms: function() {
-        for (let i = 0; i < 2; i++) {
-      const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
+	start: function () {
+		personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-      if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-      } else {
-        console.log('error');
-        i--;
-      }
-    }
-  },
+		while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
+			personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+		}
+	},
 
-  detectPersonalLevel: function() {
-        if (personalMovieDB.count < 10) {
-      console.log('Просмотрено довольно мало фильмов');
-    } else if (personalMovieDB.count < 30 && personalMovieDB >= 10) {
-      console.log('Вы классический зритель');
-    } else if (personalMovieDB.count >= 30) {
-      console.log('Вы киноман');
-    } else {
-      console.log('Произошла ошибка');
-    }
-  },
+	rememberMyFilms: function () {
+		for (let i = 0; i < 2; i++) {
+			const a = prompt('Один из последних просмотренных фильмов?', ''),
+				b = prompt('На сколько оцените его?', '');
 
-  showMyDB: function(hidden) {
-        if (!hidden) {
-      console.log(personalMovieDB);
-    }
-  },
+			if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+				personalMovieDB.movies[a] = b;
+				console.log('done');
+			} else {
+				console.log('error');
+				i--;
+			}
+		}
+	},
 
-  toggleVisibleMyDB: function () {
-    if (personalMovieDB.privat) {
-      personalMovieDB.privat = false;
-    } else {
-      personalMovieDB.privat = true;
-    }
-  },
+	detectPersonalLevel: function () {
+		if (personalMovieDB.count < 10) {
+			console.log('Просмотрено довольно мало фильмов');
+		} else if (personalMovieDB.count < 30 && personalMovieDB >= 10) {
+			console.log('Вы классический зритель');
+		} else if (personalMovieDB.count >= 30) {
+			console.log('Вы киноман');
+		} else {
+			console.log('Произошла ошибка');
+		}
+	},
 
-  writeYourGenres: function() {
-    for (let i = 1; i <= 3; i++) {
-      let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+	showMyDB: function (hidden) {
+		if (!hidden) {
+			console.log(personalMovieDB);
+		}
+	},
 
-      if (genre == '' || genre == null) {
-        console.log('Вы ввели некорректные данные или не ввели их вовсе');
-        i--;
-      } else {
-        personalMovieDB.genres[i - 1] = genre;
-      }
-    }
+	toggleVisibleMyDB: function () {
+		if (personalMovieDB.privat) {
+			personalMovieDB.privat = false;
+		} else {
+			personalMovieDB.privat = true;
+		}
+	},
 
-    personalMovieDB.genres.forEach((item, i) => {
-      console.log(`Любимый жанр ${i + 1} - ${item}`);
-    });
-  }
+	writeYourGenres: function () {
+		for (let i = 1; i <= 3; i++) {
+			let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+
+			if (genre == '' || genre == null) {
+				console.log('Вы ввели некорректные данные или не ввели их вовсе');
+				i--;
+			} else {
+				personalMovieDB.genres[i - 1] = genre;
+			}
+		}
+
+		personalMovieDB.genres.forEach((item, i) => {
+			console.log(`Любимый жанр ${i + 1} - ${item}`);
+		});
+	},
 };
-
-
-
 
 // Альтернативный способ задания №3
 
